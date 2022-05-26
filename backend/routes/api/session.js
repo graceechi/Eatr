770991436ff05561538e94ddd55225f07a30df97
Grace Chi <gracetrc7@gmail.com>
2022-05-26 16:32:10 -0500

    phase 4 user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,4 +30,23 @@ router.post(
     })
 );
 
+// Restore session user
+router.get("/", restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject(),
+      });
+    } else return res.json({});
+});
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
+
 module.exports = router;
