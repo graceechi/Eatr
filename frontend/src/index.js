191f7f1f2e426edd0adf513c2579b4459427c6fe
Grace Chi <gracetrc7@gmail.com>
2022-05-26 17:56:36 -0500

    initial react set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-
-import './index.css';
-
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
-
 import configureStore from './store';
 
 const store = configureStore();
@@ -16,18 +13,18 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 function Root() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Root />
-    </React.StrictMode>,
-    document.getElementById('root'),
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
