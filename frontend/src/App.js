4035c795b1021d4af0651ef460591468b50a3efc
Grace Chi <gracetrc7@gmail.com>
2022-05-27 18:43:38 -0500

    login restoreUser session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from 'react-router-dom';
+import * as sessionActions from "./store/session";
+
 // import LoginFormContainer from './components/LoginForm';
 
 function App() {
-  return (
-    <h1>hi</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
+    <h1>hi</h1>
+    // <Switch>
+    //   <Route path="/login">
+    //     <LoginFormPage />
+    //   </Route>
+    // </Switch>
   );
 }
 
