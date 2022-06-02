a42b85566bde365ec9172ad1ef6dce4626b3d20a
Grace Chi <gracetrc7@gmail.com>
2022-06-02 13:16:14 -0500

    reducer for view comments

---
+++ frontend/src/store/comments.js
@@ -0,0 +1,37 @@
+import { csrfFetch } from './csrf';
+
+const VIEW_COMMENTS = '/photos/VIEW_COMMENTS';
+// const ADD_COMMENT = 'photos/ADD_COMMENT';
+// const UPDATE_COMMENT = '/photos/UPDATE_COMMENT';
+// const REMOVE_COMMENT = '/photos/REMOVE_COMMENT';
+
+const viewComments = comments => ({
+    type: VIEW_COMMENTS,
+    comments
+})
+
+export const loadComments = id => async dispatch => {
+    const res = await csrfFetch(`/api/comments/photos/${id}`);
+    if (res.ok) {
+        const comments = await res.json();
+        dispatch(viewComments(comments));
+    }
+}
+
+const initialState = { entries: {}, isLoading: true }
+
+const commentsReducer = (state = initialState, action) => {
+    // let newState = {};
+    switch (action.type) {
+        case VIEW_COMMENTS: {
+            const newState = { ...state, entries: {...state.entries} };
+            action.comments.forEach(comment => {
+                newState.entries[comment.id] = comment;
+            });
+          return newState;
+        }
+        default:
+            return state;
+    }
+}
+export default commentsReducer;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
 import photosReducer from './photos';
+import commentsReducer from './comments';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  photos: photosReducer
+  photos: photosReducer,
+  comments: commentsReducer
 });
 
 let enhancer;
